04096b7b9f040ad5e8cda6a02efefe3f32ae95b0
Lucio Charallo <hi@lucio.app>
2020-07-29 01:52:30 -0300

    added check to differents hashs

--- lib/score_diff.rb
+++ lib/score_diff.rb
@@ -4,5 +4,21 @@ class ScoreDiff
 
   def self.calculate_hash_score(a, b)
     return 0 if a == b
+
+    partial_result = { keys_count: 0, differences: 0 }
+    deep_diff(a, b, partial_result)
+    puts "partial_result: #{partial_result} result: #{partial_result[:differences].to_f / partial_result[:keys_count]}"
+    partial_result[:differences].to_f / partial_result[:keys_count]
+  end
+
+  def self.deep_diff(a, b, partial_result)
+    partial_result[:keys_count] += (a.keys | b.keys).length
+    (a.keys | b.keys).each do |k|
+      next if a[k] == b[k]
+
+      partial_result[:differences] += 1
+      partial_result = deep_diff(a[k], b[k], partial_result) if a[k].is_a?(Hash) && b[k].is_a?(Hash)
+    end
+    partial_result
   end
 end
--- spec/score_diff_spec.rb
+++ spec/score_diff_spec.rb
@@ -13,5 +13,51 @@ RSpec.describe ScoreDiff do
         expect(subject).to eq 0
       end
     end
+
+    context 'when hash is completely different' do
+      let(:hash_b) { { x: 1, y: 2, z: 3 } }
+
+      it 'should be one' do
+        expect(subject).to eq 1
+      end
+    end
+
+    context 'when hash is partial different' do
+      let(:hash_b) { { a: 1, b: [1, 'a'], c: 2 } }
+
+      it 'should be the percentage of compatibility' do
+        expect(subject).to eq 0.5
+      end
+    end
+  end
+
+  describe '#deep_diff' do
+    let(:partial_result) { { differences: 0, keys_count: 0 } }
+
+    subject { ScoreDiff.deep_diff(hash_a, hash_b, partial_result) }
+
+    context 'when hash is equals' do
+      let(:hash_b) { hash_a }
+
+      it 'partial result does not change' do
+        expect(subject).to eq partial_result
+      end
+    end
+
+    context 'when hash is completely different' do
+      let(:hash_b) { { x: 1, y: 2, z: 3 } }
+
+      it 'total of differences are equal to total of keys' do
+        expect(subject).to eq({ differences: 7, keys_count: 7 })
+      end
+    end
+
+    context 'when hash is partial different' do
+      let(:hash_b) { { a: 1, b: [1, 'a'], c: 2 } }
+
+      it 'should be the percentage of compatibility' do
+        expect(subject).to eq({ differences: 2, keys_count: 4 })
+      end
+    end
   end
 end
